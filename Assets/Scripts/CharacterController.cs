17f97098103b1db69214d86776bede601307f241
RonixKuinn <154350005+RonixKuinn@users.noreply.github.com>
2024-11-22 08:47:41 +0100

    camarahechacreo

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    private characterMove _vertical;
+    private characterMovement _horizontal;
+    
+
+    void Awake()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
